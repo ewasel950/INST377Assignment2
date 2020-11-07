33586fac7be5090986ffc9f412b50a78e2587b6a
Eman Wasel <ewasel@terpmail.umd.edu>
2020-11-07 09:32:00 -0500

    update script.js

---
+++ public/script.js
@@ -0,0 +1,19 @@
+
+
+//main function 
+document.body.addEventListener(async (e) => {
+    e.preventDefault(); // this stops whatever the browser wanted to do itself.
+    const form = $(e.target).serializeArray();
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+    })
+      .then((fromServer) => fromServer.json())
+      .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
+      .catch((err) => {
+        console.log(err);
+      });
+  });
