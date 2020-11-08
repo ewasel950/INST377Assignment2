419dac7f3bb9d59807f830f89f259bd33ba8f946
RavendraNand97 <ravendranand97@gmail.com>
2020-11-08 17:24:48 -0500

    Update to JS search and list css

--- public/script.js
+++ public/script.js
@@ -1,25 +1,6 @@
 
-function range(int) {
-    const arr = [];
-    for (let i = 0; i < int; i += 1) {
-      arr.push(i);
-    }
-    return arr;
-  }
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); 
-  }  
-
-  async function main(){
-    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-    const json = await data.json;
-    console.log('data from fetch', json);
-    res.json(json);
-  }
-
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
 const cities = [];
 
 fetch(endpoint)
@@ -28,37 +9,34 @@ fetch(endpoint)
 
 function findMatches(wordToMatch, cities){
   while (wordToMatch.length === 0){
-     cities = []
-     return cities;}
-  
-     return cities.filter(place => {
+    cities = []
+    return cities;
+  }
+
+    return cities.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi');
-       return place.city.match(regex) || place.state.match(regex) || place.zip.match(regex);
+       return place.city.match(regex) || place.zip.match(regex)|| place.name.match(regex);
     });
 }
 function displayMatches(){
     const matchArray = findMatches(this.value, cities);
-    const html = matchArray.map(place => `
-    <div class ="card card-body mb-1">
-     <h4>${place.name} 
-         ${place.city}
-          <span
-         class ="text-primary"> ${place.zip}</span></h4></div>
-          `
-     ) 
-     /* return `
-            <ul>
-              <li class = "name"> ${place.name}</li>
-              <li class = "name"> ${place.city}</li>
-              <li class = "name"> ${place.zip}</li>
-            </ul>
-        `;*/
-    .join('');
+    const html = matchArray.map(place => {
+
+        return `
+            <li>
+              <span class = 'name'> ${place.name}</span><br>
+              <span class = 'city'> ${place.city}</span><br>
+              <span class = 'zip'> ${place.zip} </span>
+            </li>
+        `;
+    }).join('');
     suggestions.innerHTML = html;
-  
+    
+}
 
 const searchInput = document.querySelector('.textinput');
 const suggestions = document.querySelector('.suggestions')
 
 searchInput.addEventListener('keyup', displayMatches);
-}
+
+
